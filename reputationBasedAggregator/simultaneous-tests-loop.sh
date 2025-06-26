81abc6ac521377061156e5f2eba0e7bcca2d01eb
yenachar <yenachar@protonmail.com>
2025-06-25 23:45:44 -0400

    Added a one-minute sleep between sets of simultaneous runs to allow taxed arbiters to clear.

--- reputationBasedAggregator/simultaneous-tests-loop.sh
+++ reputationBasedAggregator/simultaneous-tests-loop.sh
@@ -1,1 +1,1 @@
-for i in {1..5}; do echo "=== Test Run $i at $(date) ==="; ./simultaneous-tests.sh; echo ""; done > test_results.log 2>&1
+for i in {1..10}; do echo "=== Test Run $i at $(date) ==="; ./simultaneous-tests.sh; echo ""; sleep 60; done > test_results.log 2>&1
