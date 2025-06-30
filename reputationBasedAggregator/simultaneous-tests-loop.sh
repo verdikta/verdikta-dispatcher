7ff9e9dbf37275c19cb4d1485e62f4be04ba6cc7
yenachar <yenachar@protonmail.com>
2025-06-29 22:05:11 -0400

    Cut the pause between sets of runs to 30s.

--- reputationBasedAggregator/simultaneous-tests-loop.sh
+++ reputationBasedAggregator/simultaneous-tests-loop.sh
@@ -1,1 +1,1 @@
-for i in {1..10}; do echo "=== Test Run $i at $(date) ==="; ./simultaneous-tests.sh; echo ""; sleep 60; done > test_results.log 2>&1
+for i in {1..10}; do echo "=== Test Run $i at $(date) ==="; ./simultaneous-tests.sh; echo ""; sleep 30; done > test_results.log 2>&1
