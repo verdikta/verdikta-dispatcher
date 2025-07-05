4a9484c7f9dbcd13f2e6918e035ff4e761674b0e
yenachar <yenachar@protonmail.com>
2025-07-05 00:08:04 -0400

    Updated.

--- demoClient/query-demo.sh
+++ demoClient/query-demo.sh
@@ -1,1 +1,1 @@
-node scripts/query-demo.js 0x8a56E7D23414dD793D322d4783336A9bd29E773B
+node scripts/query-demo.js 0x15B3324A52853B56682a4E1470f7Ec3d86578a0F
--- demoClient/transfer-link.sh
+++ demoClient/transfer-link.sh
@@ -1,2 +1,2 @@
-node scripts/transfer-link.js 0x8a56E7D23414dD793D322d4783336A9bd29E773B 0.15
+node scripts/transfer-link.js 0x15B3324A52853B56682a4E1470f7Ec3d86578a0F 0.10
 
