e4ab834166dbd970f6af21a3c0cdb27c7aebcc6d
yenachar <yenachar@protonmail.com>
2025-07-23 23:44:30 -0400

    Updated compiler pragma to require 0.8.23 or higher to avoid old compiler bug.

--- reputationBasedAggregator/contracts/ReputationAggregator.sol
+++ reputationBasedAggregator/contracts/ReputationAggregator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.23;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
