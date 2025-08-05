6bcdedad02ed65c61662fa6317521682d9345e88
yenachar <yenachar@protonmail.com>
2025-08-04 23:21:26 -0400

    Extended the script to accept an (optional) aggId that overrides the default.

--- reputationBasedAggregator/agg-history.sh
+++ reputationBasedAggregator/agg-history.sh
@@ -1,4 +1,4 @@
 HARDHAT_NETWORK=base_sepolia \
 node scripts/agg-history.js \
-  --aggregator 0x2bF73a372CA04C30e9a689BAc4BfC976DfBEb504 \
-  --aggid      0x443c87545b2a3722f7e1665a730e2f9deffdeee2db951e50e9d78ab636635c23
+  --aggregator 0x6a26f45D5BbFC3AEEd8De9bd2B8285b96554bC47 \
+  --aggid      ${1:-0x99c0235f1c034cd47c382d73acad2931fb7f54e46e879252b346a895c59aa85a}
